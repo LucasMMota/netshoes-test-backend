69f6dd1f8a93969ca8250f338684eac1f7190cc8
Lucas Mendes <lucas.lmmf@gmail.com>
2018-04-07 12:58:18 -0300

    Add comentarios

--- src/main/java/com/netshoes/problems/problem1/impl/SearchVowel.java
+++ src/main/java/com/netshoes/problems/problem1/impl/SearchVowel.java
@@ -5,49 +5,52 @@ import com.netshoes.problems.problem1.api.Stream;
 // NAO RENOMEAR ESSA CLASSE!
 public class SearchVowel {
 
-    // Implementei nos testes a lógica da resolução que imaginei. O teste não rodou devido a erros técnicos da linguagem.
-    // procurei mostrar como eu pensei em resolver
+    // Implementei nos testes a lógica da resolução que imaginei. 
+    // O teste não rodou devido a erros técnicos que encontrei ao utilizar a linguagem JAVA.
+    // Procurei mostrar a maneira a qual eu pensei em resolver o problema
 
     // NAO ALTERAR ESSA ASSINATURA!
     public static char firstChar(Stream input) {
         // TODO sua implementacao! Nesse ponto deve ser feita a adaptacoes necessarias para que os testes executem com sucesso
         
-    	while(input.hasNext()){
-			char nextChar = input.getNext();
-			int posLida = input.qtdLido;;
-			
-			if (this.isVowel(nextChar) && this.temConsoanteEVogalAntes(input, posLida) && this.naoEhRepetido(nextChar, input)){
-				return nextChar;
-			}
-    		
-    	}
+        // itera os elementos char da string
+        while(input.hasNext()){
+            char nextChar = input.getNext();
+            int posLida = input.qtdLido;;
+            
+            if (this.isVowel(nextChar) && this.temConsoanteEVogalAntes(input, posLida) && this.naoEhRepetido(nextChar, input)){
+                return nextChar;
+            }
+            
+        }
 
-        return '0'; 
+        return '0';  // caso não satisfaça as regras, retorna 0;
     }
 
     // verifica a segunda parte da regra
     public boolean temConsoanteEVogalAntes(Stream input, int charPos){
-    	return isConsonant(input.stream.charAt(charPos-1)) && isVowel(input.stream.charAt(charPos-2));
+        return isConsonant(input.stream.charAt(charPos-1)) && isVowel(input.stream.charAt(charPos-2));
     }
 
+    // itera o array para checar se a vogal nao esta repetida
     public boolean naoEhRepetido(char nextChar, Stream input){
-    	for(int i=input.qtdLido+1; i<input.stream.length(); i++){
-    		if(input.stream.charAt(i)==nextChar){
-    			return false;
-    		}
-		}
+        for(int i=input.qtdLido+1; i<input.stream.length(); i++){
+            if(input.stream.charAt(i)==nextChar){
+                return false;
+            }
+        }
 
-		return true;
+        return true;
     }
 
-	public static boolean isVowel(char c){
+    public static boolean isVowel(char c){
     String vowels = "aeiouAEIOU";
     return vowels.contains(c);
-	}
+    }
 
-	public static boolean isConsonant(char c){
-	    String cons = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
-	    return cons.contains(c);
-	}
+    public static boolean isConsonant(char c){
+        String cons = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+        return cons.contains(c);
+    }
 
 }
--- src/main/java/com/netshoes/problems/problem2/impl/CompareVersionImpl.java
+++ src/main/java/com/netshoes/problems/problem2/impl/CompareVersionImpl.java
@@ -3,9 +3,9 @@ package com.netshoes.problems.problem2.impl;
 // Nao alterar o nome dessa classe!
 public class CompareVersionImpl {
 
-
-	// Implementei nos testes a lógica da resolução que imaginei. O teste não rodou devido a erros técnicos da linguagem.
-    // procurei mostrar como eu pensei em resolver
+	// Implementei nos testes a lógica da resolução que imaginei. 
+    // O teste não rodou devido a erros técnicos que encontrei ao utilizar a linguagem JAVA.
+    // Procurei mostrar a maneira a qual eu pensei em resolver o problema
 
     // Nao alterar a assinatura desse metodo
     public static int compareVersion(String version1, String version2) {
@@ -15,15 +15,17 @@ public class CompareVersionImpl {
         // para cada uma verificar se a respectiva posição no outro array é menor ou maior e retornar
         // caso sejam iguais, avança uma posição em ambos e verifica novamente
 
-
+    	// explode a string em dois arrays
 		String[] arrV1 = version1.split(".");
 		String[] arrV2 = version2.split(".");
 	
+		// pego o tamanho do maior para fazer o looping
 		int tamIteracaco = arrV2.length;
-		if (arrV1.length > arrV2.length){
+		if (arrV1.length > tamIteracaco){
 			tamIteracaco = arrV1.lenght;
 		}
 
+		// itero os arrays e verifico as versoes
 		for (int i = 0; i < tamIteracao; i++) {
 			if(this.getVal(arrV1,i)>this.getVal(arrV2,i)){
 				return 1;
@@ -37,6 +39,7 @@ public class CompareVersionImpl {
 		return 0; // sao iguais
 	}
 
+	// usei essa funcao para retornar 0 quando um array for menor
 	public int getVal(String[] arr, int i){
 		if(i>=arr.length){
 			return 0; //igualiza o array
